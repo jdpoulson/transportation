dbfe39c07f386ed4b3d1a1296a82419f2ce84a02
John Poulson <Jdpoulson@gmail.com>
2019-02-06 14:59:09 -0800

    created a transportation website

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("form#transportation_survey").submit(function(event){
+    event.preventDefault();
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      var workTransportationMode = $(this).val();
+      $('#work-responses').append(workTransportationMode + "<br>");
+    });
+    $('#transportation_survey').hide();
+  });
+});
