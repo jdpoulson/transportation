0797c9ac73c8e3cb43a50feca3cb732ab18459d9
John Poulson <Jdpoulson@gmail.com>
2019-02-08 13:28:02 -0800

    added changes to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,15 @@ $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
     $("#work-responses").show();
+    var userResponses = [];
     $("input:checkbox[name=work-transportation]:checked").each(function(){
       var workTransportationMode = $(this).val();
       $('#work-responses').append(workTransportationMode + "<br>");
+    });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
     });
     $('#transportation_survey').hide();
   });
